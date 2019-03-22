955cf0e6bce34ae94d9a390530a3d0b412037d24
SearheyKuvizenkau <48785611+SearheyKuvizenkau@users.noreply.github.com>
2019-03-22 22:02:28 +0300

    Update ArrayTask.java

--- Task3/src/com/stormnet/ArrayTask.java
+++ Task3/src/com/stormnet/ArrayTask.java
@@ -5,20 +5,28 @@ import java.util.Scanner;
 public class ArrayTask {
 
     public static void main(String[] args) {
-
-        int[] array = new int[8];
+        Scanner scanner = new Scanner(System.in);
+        Integer a = scanner.nextInt();
+        System.out.println(a);
+        
+        int [] array = new int[a];
         int sum = 0;
         float average;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the numbers");
+         Scanner scanne = new Scanner(System.in);
+         System.out.println("Введите числа");
 
+         for(int i=0; i<array.length; i++){
+
+            array[i] = scanne.nextInt();
+
+            System.out.println(array[i]);
+
+        }
+        
         for(int i=0; i<array.length; i++){
 
-            array[i] = scanner.nextInt();
-
             sum+=array[i];
-
         }
 
         average = (float) sum/array.length;
