dcea901726d57f9a7a11fe48ea683472d8c0adf5
SearheyKuvizenkau <48785611+SearheyKuvizenkau@users.noreply.github.com>
2019-04-10 15:01:17 +0300

    Update CheckService.java

--- Task6/Task6/src/com/stormnet/CheckService.java
+++ Task6/Task6/src/com/stormnet/CheckService.java
@@ -1,9 +1,20 @@
 package com.stormnet;
 
-public class CheckService {
-    public static int arrObjects(int i, int [] CheckPosition) {
-        if (i == (CheckPosition.length - 1))
-            return CheckPosition[i];
-        return CheckPosition[i] + arrObjects(i + 1,CheckPosition);
-        }
+public class Main {
+    
+    public static void main(String[] args) {
+
+        CheckPosition[] checkPositions = new CheckPosition[2];
+
+        CheckPosition check1 = new CheckPosition("Beer", 1.0, 2.8);
+        CheckPosition check2 = new CheckPosition("Cheeps", 1.0, 1.8);
+
+
+        checkPositions[0] = check1;
+        checkPositions[1] = check2;
+
+        CheckService checkservice = new CheckService();
+
+        System.out.print(checkservice.getCheckPrice(checkPositions));
     }
+}
