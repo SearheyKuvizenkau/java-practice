cfef02ce1673eec6b27843687d0581431915f19f
SearheyKuvizenkau <48785611+SearheyKuvizenkau@users.noreply.github.com>
2019-04-21 17:57:22 +0300

    Update Main.java

--- Task7/src/com/stormnet/Main.java
+++ Task7/src/com/stormnet/Main.java
@@ -3,60 +3,43 @@ package com.stormnet;
 
 public class Main {
 
-    public static void main(String[] args) throws IllegalArgumentException {
+    public static void main(String[] args) {
 
-        String Str1 = "-c";
-        String Str2 = args[0];
+        try {
 
-        boolean cirVal;
+            if (args[0].equals("-c") && args.length == 2) {
 
-        cirVal = Str1.equals(Str2);
+                try {
+                    Shape shape = new Circle(Double.parseDouble(args[1]));
+                    System.out.println(args[0] + " " + shape.perimetr());
+                    System.out.println(args[0] + " " + shape.square());
+                } catch (IllegalArgumentException e) {
+                    System.out.print("Вы ввели отрицательное значение, либо равное нулю:" + " " + e);
+                }
+            }
 
+            if (args[0].equals("-r") && args.length == 3) {
 
-        if (cirVal == true && args.length == 2) {
+                try {
+                    Shape shape = new Rectangle(Double.parseDouble(args[1]), Double.parseDouble(args[2]));
+                    System.out.println(args[0] + " " + shape.perimetr());
+                    System.out.println(args[0] + " " + shape.square());
+                } catch (IllegalArgumentException e) {
+                    System.out.print("Вы ввели отрицательные значения, либо равные нулю:" + " " + e);
+                }
+            }
+            
+            if (args[0].equals("-h") && args.length == 1) {
 
-            Shape shape = new Circle(Integer.parseInt(args[1]));
+                System.out.print("Помощь: Программа должна принимать следующие аргуметны:" +
+                        " -с r, где r - радиус круга;" +
+                        " -r a b, где а и b стороны прямоугольника");
 
-            if (Integer.parseInt(args[1]) <= 0) throw new IllegalArgumentException("Вы ввели отрицательные значения," +
-                    " либо равные нулю");
+            }
 
-            System.out.println(args[0] + " " + shape.perimetr());
-            System.out.println(args[0] + " " + shape.square());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.print("Не были заданы аргументы" + e);
         }
-
-        String Str3 = "-r";
-        String Str4 = args[0];
-
-        boolean rectVal;
-
-        rectVal = Str3.equals(Str4);
-
-        if (rectVal == true && args.length == 3) {
-
-            Shape shape = new Rectangle(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-
-            if (Integer.parseInt(args[1]) <= 0 || Integer.parseInt(args[2]) <= 0)
-                throw new IllegalArgumentException("Вы ввели отрицательные значения, либо равные нулю.");
-
-            System.out.println(args[0] + " " + shape.perimetr());
-            System.out.println(args[0] + " " + shape.square());
-        }
-
-        String Str5 = "-h";
-        String Str6 = args[0];
-
-        boolean helpVal;
-
-        helpVal = Str5.equals(Str6);
-
-        if (helpVal == true && args.length == 1) {
-
-            System.out.print("Помощь: Введите положительные числа для соответствующей фигуры.");
-
-        }
-
     }
-
 }
-
 
